5951b4b6aff9d802dd19dabd259ad59c0d8deafe
Paul Houser <m.houser.p@gmail.com>
2020-02-12 11:43:30 -0800

    updated install script

--- install.sh
+++ install.sh
@@ -2,3 +2,19 @@
 FILEPATH=$(readlink -f "theme.sh")
 
 ln -s $FILEPATH /usr/local/bin/chameleon
+
+# make the directory for the lightdm background image with the correct permissions.
+if ! [ -d /usr/share/wallpapers ]; then
+  mkdir /usr/share/wallpapers
+fi
+chmod 777 /usr/share/wallpapers
+
+# generate the config for slick-greeter if it doesn't already exist
+slickconf="/etc/lightdm/slick-greeter.conf"
+if ! [[ -f $slickconf ]]; then
+  touch $slickconf
+  echo "[Greeter]" >> $slickconf
+  echo "background=/usr/share/wallpapers/wal"
+else
+  sed -i.chmlnbk "s/background=.*/background=\/usr\/share\/wallpapers\/wal/g" /etc/lightdm/slick-greeter.conf
+fi
