c54190e677038915fc2f94fddce2069556b68304
gideonwolfe <wolfeg2@wwu.edu>
2020-08-26 13:19:36 -0700

    added pip installation

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ FILEPATH=$(readlink -f "chameleon.py")
 ln -s $FILEPATH $HOME/.local/bin/chameleon.py
 echo "chameleon.py has been linked to $HOME/.local/bin/"
 echo "Ensure this directory is in your $PATH"
+
+echo "Downloading pip dependencies"
+pip install --user whichcraft
