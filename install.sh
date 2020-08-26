a9629dbf39e14add45a4be036a79939ff68be3d7
gideonwolfe <wolfeg2@wwu.edu>
2020-08-26 13:18:07 -0700

    updated install script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-FILEPATH=$(readlink -f "theme.sh")
-
-ln -s $FILEPATH /usr/local/bin/chameleon
+FILEPATH=$(readlink -f "chameleon.py")
+ln -s $FILEPATH $HOME/.local/bin/chameleon.py
+echo "chameleon.py has been linked to $HOME/.local/bin/"
+echo "Ensure this directory is in your $PATH"
